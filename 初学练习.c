ed741c75cf25931111ce9790685ff60147ade0c5
WuM3 <91886168+WuM3@users.noreply.github.com>
2021-10-08 23:34:48 +0800

    Add files via upload

---
+++ 初学练习.c
@@ -0,0 +1,53 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int a ;
+//	int c;
+//	int sum ;
+//	scanf("%d %d", &a, &c);
+//	sum = a + c;
+//	printf("sum=%d\n", sum);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("¹þ¹þ");
+//	return 0;
+//}
+//#include <stdio.h>
+//extern a;
+//int main()
+//{
+//	printf("%d\n", a);
+//	return 0;
+//}
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	float a ;
+//	float b ;
+//	float c ;
+//	float sum ;
+//	scanf("%f %f %f", &a, &b, &c);
+//	sum = a + b + c;
+//	printf("sum=%f\n", sum);
+//
+//
+//	return 0;
+//}
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int r;
+//	float C;
+//	printf("ÇëÊäÈë°ë¾¶r=");
+//	scanf("%d", &r);
+//	C = 2 * 3.14 * r;
+//	printf("ÖÜ³¤C=%f\n",C);
+//	return 0;
+//}
