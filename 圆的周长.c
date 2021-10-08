aa574e09bdcd6937ab64e12a84b5e71c82f49819
WuM3 <91886168+WuM3@users.noreply.github.com>
2021-10-08 23:36:36 +0800

    Add files via upload

---
+++ 圆的周长.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int r;
+	float C;
+	printf("«Î ‰»Î∞Îæ∂r=");
+	scanf("%d", &r);
+	C = 2 * 3.14 * r;
+	printf("÷‹≥§C=%f",C);
+	return 0;
+}
